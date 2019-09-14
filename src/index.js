fca91f2c0c8eaa3608ac5641c6ecc3c0865898bd
ANDYGE <geshaofei@126.com>
2019-09-14 20:36:17 +0800

    lodash
    odd arr

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import _ from "lodash";
 import "./styles.css";
 
 function App() {
+  const arr = _.filter([1, 2, 3, 4, 5, 6], i => i % 2 === 0);
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <div>{arr.join()}</div>
     </div>
   );
 }
